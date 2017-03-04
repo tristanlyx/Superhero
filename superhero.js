4e7dfd59c4276e37323a5cdd283e332870897279
METROSTATE\36BDPA3 <tristanlyx@gmail.com>
2017-03-04 14:06:32 -0600

    hello?

--- superhero.js
+++ superhero.js
@@ -1,6 +1,3 @@
 function showSecretIdentity(){
-    alert("I may look like a squid, but I'm really an alien.");
-}
-function catchPhraseText(){
-	alert("Gotta catch 'em all!!");
+    alert("I'm just a regular squid...");
 }
